048aba6b7dd3864fb8e9606c48021996f84095bc
charmiidalal <dalal.c@northeastern.edu>
2022-03-22 15:16:40 -0700

    write testcases to make sure correct output

--- assignment-movie-rating-analysis/src/main/scala/edu/neu/coe/csye7200/asstsmra/MovieRating.scala
+++ assignment-movie-rating-analysis/src/main/scala/edu/neu/coe/csye7200/asstsmra/MovieRating.scala
@@ -3,32 +3,62 @@ package edu.neu.coe.csye7200.asstsmra
 import org.apache.spark.sql.{DataFrame, SparkSession, functions}
 
 /**
- * @author Yanda Yuan
+ * @author Charmi Dalal
  * Compare different methods on different functions, which will be better on your project?
  * wordCountSpec can be used as an example if you don't know what the result will be like.
  * To help you understand more about the difference among functions: http://homepage.cs.latrobe.edu.au/zhe/ZhenHeSparkRDDAPIExamples.html
  */
-object movieRating extends App {
+
+class MovieRating
+{
   val spark: SparkSession = SparkSession
-    .builder()
-    .appName("MovieRating")
-    .master("local[*]")
-    .getOrCreate()
+  .builder()
+  .appName("MovieRating")
+  .master("local[*]")
+  .getOrCreate()
   spark.sparkContext.setLogLevel("ERROR")
 
-  val movieMetadata: DataFrame  = spark.read.format("csv")
+  val movieMetadata: DataFrame = spark.read.format("csv")
     .option("inferSchema", "true")
     .option("header", "true")
     .load("src/main/resources/edu/neu/coe/csye7200/asstsmra/movie_metadata.csv")
 
-  val movie_analysis : DataFrame  = movieMetadata.select("movie_title", "imdb_score").groupBy("movie_title").agg(functions.avg("imdb_score"))
-  val movie_deviation : DataFrame  = movieMetadata.select("imdb_score").agg(functions.stddev("imdb_score"))
-
-  println(s"Mean of Movie rating by Movie Title")
-  println(s"*************************************")
-  for(movie_mean <-movie_analysis.collect()){
-    println(s"Mean of Rating of ${movie_mean(0)} -  ${movie_mean(1)}")
+  def meanMovieRating() : DataFrame = {
+    val movie_analysis: DataFrame = movieMetadata.select("movie_title", "imdb_score").groupBy("movie_title").agg(functions.avg("imdb_score"))
+    println(s"Mean of Movie rating by Movie Title")
+    println(s"*************************************")
+    for (movie_mean <- movie_analysis.collect()) {
+      println(s"Mean of Rating of ${movie_mean(0)} -  ${movie_mean(1)}")
+    }
+    movie_analysis
   }
-  println(s"*************************************")
-  println(s"Standard Deviation of Movie Rating for All Movies: ${movie_deviation.collect()(0)}")
+
+  def meanMovieRatingByAll() : Any = {
+    val movie_analysis: DataFrame = movieMetadata.select( "imdb_score").agg(functions.avg("imdb_score"))
+    println(s"*************************************")
+    println(s"Mean of Movie rating by All Movies:  ${movie_analysis.collect()(0).get(0)}")
+
+    movie_analysis.collect()(0).get(0)
+  }
+
+
+  def standardDevMovieRating(): Any = {
+    val movie_deviation: DataFrame = movieMetadata.select("imdb_score").agg(functions.stddev("imdb_score"))
+
+    println(s"*************************************")
+    println(s"Standard Deviation of Movie Rating for All Movies: ${movie_deviation.collect()(0).get(0)}")
+
+    movie_deviation.collect()(0).get(0)
+  }
+}
+
+object MovieRatingObj extends MovieRating
+{
+  def main(args:Array[String])
+  {
+    val mr = new MovieRating()
+    mr.meanMovieRating()
+    mr.meanMovieRatingByAll()
+    mr.standardDevMovieRating()
+  }
 }
--- assignment-movie-rating-analysis/src/test/scala/edu/neu/coe/csye7200/asstswc/MovieRatingSpec.scala
+++ assignment-movie-rating-analysis/src/test/scala/edu/neu/coe/csye7200/asstswc/MovieRatingSpec.scala
@@ -1,92 +1,55 @@
 package edu.neu.coe.csye7200.asstswc
 
-import org.apache.spark.sql.SparkSession
+import edu.neu.coe.csye7200.asstsmra.MovieRatingObj
+import org.apache.spark.sql.{SparkSession}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.tagobjects.Slow
 import org.scalatest.{BeforeAndAfter, flatspec}
+
+import scala.io.Source
 import scala.util.Try
-//
-///**
-// * @author Yanda Yuan
-// */
-//class WordCountReduceByKeySpec extends flatspec.AnyFlatSpec with Matchers with BeforeAndAfter  {
-//
-//  implicit var spark: SparkSession = _
-//
-//  before {
-//    spark = SparkSession
-//      .builder()
-//      .appName("WordCount")
-//      .master("local[*]")
-//      .getOrCreate()
-//    spark.sparkContext.setLogLevel("ERROR")
-//  }
-//
-//  after {
-//    if (spark != null) {
-//      spark.stop()
-//    }
-//  }
-//
-//  behavior of "Spark"
-//
-//  it should "aggregateByKey work for wordCount" taggedAs Slow in {
-//    val triedPath = Try(getClass.getResource("WordCount.txt").getPath)
-//    triedPath.isSuccess shouldBe true
-//    for (path <- triedPath)
-//    WordCount_aggregateByKey.wordCount(spark.read.textFile(path).rdd, " ").collect() should matchPattern {
-//      case Array(("Hello", 3), ("World", 3), ("Hi", 1)) =>
-//    }
-//  }
-//  it should "combineByKey work for wordCount" taggedAs Slow in {
-//    val triedPath = Try(getClass.getResource("WordCount.txt").getPath)
-//    triedPath.isSuccess shouldBe true
-//    for (path <- triedPath)
-//      WordCount_combineByKey.wordCount(spark.read.textFile(path).rdd," ").collect() should matchPattern {
-//        case Array(("Hello",3),("World",3),("Hi",1)) =>
-//      }
-//  }
-//  it should "countByValue work for wordCount" taggedAs Slow in {
-//    val expected = Map("Hello" -> 3, "World" -> 3, "Hi" -> 1)
-//    val triedPath = Try(getClass.getResource("WordCount.txt").getPath)
-//    triedPath.isSuccess shouldBe true
-//    for (path <- triedPath) {
-//      val actual = WordCount_countByValue.wordCount(spark.read.textFile(path).rdd, " ")
-//      expected.equals(actual) shouldBe true
-//    }
-//  }
-//  it should "foldByKey work for wordCount" taggedAs Slow in {
-//    val triedPath = Try(getClass.getResource("WordCount.txt").getPath)
-//    triedPath.isSuccess shouldBe true
-//    for (path <- triedPath)
-//      WordCount_foldByKey.wordCount(spark.read.textFile(path).rdd," ").collect() should matchPattern {
-//        case Array(("Hello",3),("World",3),("Hi",1)) =>
-//      }
-//  }
-//  it should "groupByKey work for wordCount" taggedAs Slow in {
-//    val triedPath = Try(getClass.getResource("WordCount.txt").getPath)
-//    triedPath.isSuccess shouldBe true
-//    for (path <- triedPath)
-//      WordCount_groupByKey.wordCount(spark.read.textFile(path).rdd," ").collect() should matchPattern {
-//        case Array(("Hello",3),("World",3),("Hi",1)) =>
-//      }
-//  }
-//  it should "reduceByKey work for wordCount" taggedAs Slow in {
-//    val triedPath = Try(getClass.getResource("WordCount.txt").getPath)
-//    triedPath.isSuccess shouldBe true
-//    for (path <- triedPath)
-//      WordCount_reduceByKey.wordCount(spark.read.textFile(path).rdd," ").collect() should matchPattern {
-//        case Array(("Hello",3),("World",3),("Hi",1)) =>
-//      }
-//  }
-//  it should "ScalaVanilla work for wordCount" taggedAs Slow in {
-//    val inputList: List[String] = List("Hello World", "Hello World", "Hello World", "Hi")
-//    val expected = Map("Hello" -> 3, "World" -> 3, "Hi" -> 1)
-//    val triedPath = Try(getClass.getResource("WordCount.txt").getPath)
-//    triedPath.isSuccess shouldBe true
-//    for (path <- triedPath) {
-//      val actual = WordCount_ScalaVanilla.wordCount(inputList)
-//      expected.equals(actual) shouldBe true
-//    }
-//  }
-//}
+
+/**
+ * @author Charmi Dalal
+ */
+class MovieRatingSpec extends flatspec.AnyFlatSpec with Matchers with BeforeAndAfter  {
+
+  implicit var spark: SparkSession = _
+
+  before {
+    spark = SparkSession
+      .builder()
+      .appName("MovieRating")
+      .master("local[*]")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+  }
+
+  behavior of "Spark"
+
+  it should "return success for movie metadata valid path" taggedAs Slow in {
+    val triedPath = Try(Source.fromResource("movie_metadata.csv"))
+    triedPath.isSuccess shouldBe true
+  }
+
+  it should "return standard deviation of all movies" taggedAs Slow in {
+    MovieRatingObj.standardDevMovieRating() should matchPattern {
+      case 0.9988071293753289 =>
+    }
+  }
+
+  it should "return mean rating per movie" taggedAs Slow in {
+    val movie_mean =  MovieRatingObj.meanMovieRating().collect()
+    movie_mean should matchPattern {
+      case Array(_*) =>
+    }
+    movie_mean(0).get(0).toString.equals("Eraser")
+    movie_mean(0).get(1).equals(6.1)
+  }
+
+  it should "return mean rating for all movies" taggedAs Slow in {
+    MovieRatingObj.meanMovieRatingByAll() should matchPattern {
+      case 6.453200745804848 =>
+    }
+  }
+}
